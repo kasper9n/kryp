db0f0e81174536c8465a67ceeba682953eca9493
Kasper <kasperkh.kh@gmail.com>
2020-03-20 16:14:18 +0100

    Added page titles mixin

--- web/src/main.js
+++ web/src/main.js
@@ -5,6 +5,24 @@ import store from './store'
 
 Vue.config.productionTip = false
 
+const MetaPlugin = {
+  install (Vue, { titleTemplate }) {
+    if (!titleTemplate) titleTemplate = '%s'
+    Vue.mixin({
+      created () {
+        if (this.$options.fullTitle) {
+          document.title = this.$options.fullTitle
+        } else if (this.$options.title) {
+          document.title = titleTemplate.replace('%s', this.$options.title)
+        }
+      },
+    })
+  },
+}
+Vue.use(MetaPlugin, {
+  titleTemplate: '%s - Cryptrack',
+})
+
 new Vue({
   router,
   store,
