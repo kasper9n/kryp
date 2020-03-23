94defbd623f2db904bc3660383b9ff9a7d6fbf2a
Kasper <kasperkh.kh@gmail.com>
2020-03-23 18:03:55 +0100

    Removed chakra

--- web/src/main.js
+++ web/src/main.js
@@ -1,17 +1,9 @@
 import Vue from 'vue'
-import Chakra, {
-  ThemeProvider,
-  ColorModeProvider,
-  Box,
-  CSSReset,
-} from '@chakra-ui/vue'
 import App from '@/App.vue'
 import router from '@/router'
 import store from '@/store'
 
 Vue.config.productionTip = false
-
-Vue.use(Chakra)
 
 const MetaPlugin = {
   install (Vue, { titleTemplate }) {
@@ -34,7 +26,5 @@ Vue.use(MetaPlugin, {
 new Vue({
   router,
   store,
-  render: (h) => h(ThemeProvider, [
-    h(ColorModeProvider, [h(Box, [h(CSSReset), h(App)])]),
-  ]),
+  render: (h) => h(App),
 }).$mount('#app')
