91c8e1d9452e58860fb287d3f309db06b99e36fa
Kasper <kasperkh.kh@gmail.com>
2020-04-04 00:26:42 +0200

    Deleted vue template home page

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,16 +1,2 @@
 <template lang='pug'>
-.home
-  img(alt='Vue logo' src='../assets/logo.png')
-  HelloWorld(msg='Welcome to Your Vue.js App')
 </template>
-
-<script>
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  fullTitle: 'Cryptrack',
-  components: {
-    HelloWorld,
-  },
-}
-</script>
