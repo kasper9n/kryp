2b455cb08fad2db15fde45e10129314884f7771f
Kasper <kasperkh.kh@gmail.com>
2020-05-29 05:06:28 +0200

    /register now works

--- api/src/routes/user.js
+++ api/src/routes/user.js
@@ -3,17 +3,6 @@ const router = new Router()
 const validator = require('validator')
 const User = require("./../mongoose-models.js").User
 
-async function userExists(email) {
-  try {
-    const user = await User.findOne({ email: email })
-    if (user) return true
-    else return false
-    console.log(user)
-  } catch (error) {
-    console.log(error)
-  }
-}
-
 router.post('/register', async (ctx, next) => {
   console.log(ctx.request.body)
   let email = ctx.request.body.email
@@ -26,24 +15,27 @@ router.post('/register', async (ctx, next) => {
 
   if      (typeof password !== 'string') errors.push('password empty')
   else if (validator.isEmpty(password)) errors.push('password empty')
-  else if (!validator.isLength(password, {min: 8, max: 100})) errors.push('email invalid')
+  else if (!validator.isLength(password, {min: 8})) errors.push('password too short')
+  else if (!validator.isLength(password, {max: 100})) errors.push('password too long')
 
-  email = validator.normalizeEmail
+  email = validator.normalizeEmail(email)
 
   try {
-    if (await userExists(email)) errors.push('user exists')
+    const user = await User.findOne({ email: email })
+    if (user) errors.push('user exists')
   } catch (error) {
-    ctx.$err(5003, 'Error checking if user exists', error)
+    return ctx.$err(5003, 'Error checking if user exists', error)
   }
 
-  if (errors.length !== 0) ctx.$err(1001, 'Input error', errors)
+  if (errors.length !== 0) return ctx.$err(1001, 'Input error', errors)
 
-  // try {
-  //   await new User({ email, password }).save()
-  //   console.log('created new user')
-  // } catch (error) {
-  //   ctx.$err(5004, 'Error creating new user', error)
-  // }
+  try {
+    await new User({ email, password }).save()
+    console.log('created new user')
+  } catch (error) {
+    return ctx.$err(5004, `Error creating new user '${email}'`, error)
+  }
+
 })
 
 module.exports = router
