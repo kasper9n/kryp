57e7989f2e627252740b5090bd61d47d48213d46
Kasper <kasperkh.kh@gmail.com>
2021-02-23 18:01:15 +0100

    Remove meteor

--- server/main.js
+++
@@ -1,8 +0,0 @@
-import { Meteor } from 'meteor/meteor'
-import '../lib/Transactions.js'
-import '../lib/Wallets.js'
-import '../lib/Accounts.ts'
-
-Meteor.startup(() => {
-  // code to run on server at startup
-})
