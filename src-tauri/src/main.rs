2be70bfbeb8c0d57a3f6180c3d809c4be6296a49
Kasper <kasperkh.kh@gmail.com>
2021-12-27 16:59:05 +0100

    Update to tauri@next

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -7,7 +7,10 @@ use crate::data::St;
 use rust_decimal::{Decimal, RoundingStrategy};
 use std::thread;
 use tauri::api::{dialog, shell};
-use tauri::{command, CustomMenuItem, Manager, Menu, MenuItem, Window, WindowBuilder, WindowUrl};
+use tauri::{
+  command, CustomMenuItem, Manager, Menu, MenuEntry, MenuItem, Submenu, Window, WindowBuilder,
+  WindowUrl,
+};
 
 mod data;
 mod prices;
@@ -70,65 +73,71 @@ fn main() {
     ])
     .menu(Menu::with_items([
       #[cfg(target_os = "macos")]
-      MenuItem::new_submenu(
+      MenuEntry::Submenu(Submenu::new(
         &ctx.package_info().name,
-        [
-          MenuItem::About(ctx.package_info().name.clone()),
-          MenuItem::Separator,
-          MenuItem::Services,
-          MenuItem::Separator,
-          MenuItem::Hide,
-          MenuItem::HideOthers,
-          MenuItem::ShowAll,
-          MenuItem::Separator,
-          MenuItem::Quit,
-        ],
-      ),
-      MenuItem::new_submenu(
+        Menu::with_items([
+          MenuItem::About(ctx.package_info().name.clone()).into(),
+          MenuItem::Separator.into(),
+          MenuItem::Services.into(),
+          MenuItem::Separator.into(),
+          MenuItem::Hide.into(),
+          MenuItem::HideOthers.into(),
+          MenuItem::ShowAll.into(),
+          MenuItem::Separator.into(),
+          MenuItem::Quit.into(),
+        ]),
+      )),
+      MenuEntry::Submenu(Submenu::new(
         "File",
-        [
+        Menu::with_items([
           custom_item("New").accelerator("cmdOrControl+N").into(),
           custom_item("Open...").accelerator("cmdOrControl+O").into(),
-          MenuItem::Separator,
+          MenuItem::Separator.into(),
           custom_item("Save").accelerator("cmdOrControl+S").into(),
           custom_item("Save As...")
             .accelerator("shift+cmdOrControl+S")
             .into(),
-          MenuItem::Separator,
+          MenuItem::Separator.into(),
           custom_item("Export...")
             .accelerator("cmdOrControl+E")
             .into(),
           custom_item("Close").accelerator("cmdOrControl+W").into(),
-        ],
-      ),
-      MenuItem::new_submenu(
+        ]),
+      )),
+      MenuEntry::Submenu(Submenu::new(
         "Edit",
-        [
-          MenuItem::Undo,
-          MenuItem::Redo,
-          MenuItem::Separator,
-          MenuItem::Cut,
-          MenuItem::Copy,
-          MenuItem::Paste,
+        Menu::with_items([
+          MenuItem::Undo.into(),
+          MenuItem::Redo.into(),
+          MenuItem::Separator.into(),
+          MenuItem::Cut.into(),
+          MenuItem::Copy.into(),
+          MenuItem::Paste.into(),
           #[cfg(not(target_os = "macos"))]
-          MenuItem::Separator,
-          MenuItem::SelectAll,
-        ],
-      ),
-      MenuItem::new_submenu(
+          MenuItem::Separator.into(),
+          MenuItem::SelectAll.into(),
+        ]),
+      )),
+      MenuEntry::Submenu(Submenu::new(
         "View",
-        [
+        Menu::with_items([
           custom_item("Dashboard")
             .accelerator("cmdOrControl+1")
             .into(),
           custom_item("Transactions")
             .accelerator("cmdOrControl+2")
             .into(),
-          MenuItem::EnterFullScreen,
-        ],
-      ),
-      MenuItem::new_submenu("Window", [MenuItem::Minimize, MenuItem::Zoom]),
-      MenuItem::new_submenu("Help", [custom_item("Learn More").into()]),
+          MenuItem::EnterFullScreen.into(),
+        ]),
+      )),
+      MenuEntry::Submenu(Submenu::new(
+        "Window",
+        Menu::with_items([MenuItem::Minimize.into(), MenuItem::Zoom.into()]),
+      )),
+      MenuEntry::Submenu(Submenu::new(
+        "Help",
+        Menu::with_items([custom_item("Learn More").into()]),
+      )),
     ]))
     .on_menu_event(|event| {
       let event_name = event.menu_item_id();
